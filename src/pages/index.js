f54d05318e30bc1caaec4903e91fc8d1165488a9
reactworkspace <reactworkspace1@gmail.com>
2023-06-03 23:29:51 +0530

    Navigation Bar responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
-// import styles from '../styles/asws.module.css';
+// styles
+import './index.css';
+
 import Dashboard from './dashboard';
 import Zone from './zone';
 import Student from './student';
@@ -10,80 +12,264 @@ import Attendance from './attendance';
 import Syllabus from './syllabus';
 import Setting from './setting';
 
+// react-icons
+import { BsGridFill } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+// Logo & Icons
+import { ReactComponent as AswsLogo } from '../assets/svg/aswslogo.svg';
+import { ReactComponent as ZoneIcon } from '../assets/svg/zone.svg';
+import { ReactComponent as StudentIcon } from '../assets/svg/student.svg';
+import { ReactComponent as TeacherIcon } from '../assets/svg/teacher.svg';
+import { ReactComponent as NotesIcon } from '../assets/svg/notes.svg';
+import { ReactComponent as SettingIcons } from '../assets/svg/setting.svg';
+
+// hooks
+import { useState } from 'react';
+
 const AswsIndex = () => {
+  // hook to set the active onClick event
+  const [isActive, setActive] = useState('dashboard');
+  const handleClick = (isActiveItem) => {
+    setActive(isActiveItem);
+    if (window.innerWidth >= 320 && window.innerWidth <= 767) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  // Toggle Nav bar
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // setIsMenuOpen(false);
+
   return (
     <>
-      {/* <header className="headerSection">
-        <div></div>
-      </header> */}
       <Router>
-        <nav className="navbar">
-          <div className="logo">
-            <figure>{/* <img src="" alt="" /> */}</figure>
+        <header id="header-menu">
+          <div>
+            <GiHamburgerMenu
+              className={`icon ${isMenuOpen ? 'open' : ''}`}
+              onClick={handleMenuToggle}
+            />
           </div>
           <div>
-            <div> logo</div>
-            <div>
-              <ul>
-                <li>
-                  <Link to="/">
-                    <span>Logo</span>
+            <figure>
+              <AswsLogo />
+            </figure>
+          </div>
+          <div>Profile</div>
+        </header>
+        <div className="hamburger-menu">
+          {isMenuOpen && (
+            <ul className="popines">
+              <li>
+                <Link to="/" onClick={() => handleClick('dashboard')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'dashboard' ? 'active' : ''
+                    }`}
+                  >
+                    <BsGridFill className="nav-icons" />
                     <span>dashboard</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/zone">
-                    <span>Logo</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/zone" onClick={() => handleClick('zone')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'zone' ? 'active' : ''
+                    }`}
+                  >
+                    <ZoneIcon className="nav-icons" />
                     <span>zone</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/student">
-                    <span>Logo</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/student" onClick={() => handleClick('student')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'student' ? 'active' : ''
+                    }`}
+                  >
+                    <StudentIcon className="nav-icons" />
                     <span>student</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/teacher">
-                    <span>Logo</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/teacher" onClick={() => handleClick('teacher')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'teacher' ? 'active' : ''
+                    }`}
+                  >
+                    <TeacherIcon className="nav-icons" />
                     <span>teacher</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/attendance">
-                    <span>Logo</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/attendance"
+                  onClick={() => handleClick('attendance')}
+                >
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'attendance' ? 'active' : ''
+                    }`}
+                  >
+                    <NotesIcon className="nav-icons" />
                     <span>attendance</span>
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/syllabus">
-                    <span>Logo</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/syllabus" onClick={() => handleClick('syllabus')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'syllabus' ? 'active' : ''
+                    }`}
+                  >
+                    <NotesIcon className="nav-icons" />
                     <span>syllabus</span>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to="/setting" onClick={() => handleClick('setting')}>
+                  <div
+                    className={`flex list-button ${
+                      isActive === 'setting' ? 'active' : ''
+                    }`}
+                  >
+                    <SettingIcons className="nav-icons" />
+                    <span>setting</span>
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        <section id="body-section">
+          <nav className="navbar">
+            <div className="aswslogo">
+              <figure>
+                <AswsLogo />
+              </figure>
+            </div>
+            <div id="navbar-list">
+              <ul className="popines">
+                <li>
+                  <Link to="/" onClick={() => handleClick('dashboard')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'dashboard' ? 'active' : ''
+                      }`}
+                    >
+                      <BsGridFill className="nav-icons" />
+                      <span>dashboard</span>
+                    </div>
                   </Link>
                 </li>
                 <li>
-                  <Link to="/setting">
-                    <span>Logo</span>
-                    <span>setting</span>
+                  <Link to="/zone" onClick={() => handleClick('zone')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'zone' ? 'active' : ''
+                      }`}
+                    >
+                      <ZoneIcon className="nav-icons" />
+                      <span>zone</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/student" onClick={() => handleClick('student')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'student' ? 'active' : ''
+                      }`}
+                    >
+                      <StudentIcon className="nav-icons" />
+                      <span>student</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/teacher" onClick={() => handleClick('teacher')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'teacher' ? 'active' : ''
+                      }`}
+                    >
+                      <TeacherIcon className="nav-icons" />
+                      <span>teacher</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/attendance"
+                    onClick={() => handleClick('attendance')}
+                  >
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'attendance' ? 'active' : ''
+                      }`}
+                    >
+                      <NotesIcon className="nav-icons" />
+                      <span>attendance</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/syllabus" onClick={() => handleClick('syllabus')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'syllabus' ? 'active' : ''
+                      }`}
+                    >
+                      <NotesIcon className="nav-icons" />
+                      <span>syllabus</span>
+                    </div>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/setting" onClick={() => handleClick('setting')}>
+                    <div
+                      className={`flex list-button ${
+                        isActive === 'setting' ? 'active' : ''
+                      }`}
+                    >
+                      <SettingIcons className="nav-icons" />
+                      <span>setting</span>
+                    </div>
                   </Link>
                 </li>
               </ul>
             </div>
-          </div>
-        </nav>
-        <main>
-          <section className="main-section">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/zone" element={<Zone />} />
-              <Route path="/student" element={<Student />} />
-              <Route path="/teacher" element={<Teacher />} />
-              <Route path="/attendance" element={<Attendance />} />
-              <Route path="/syllabus" element={<Syllabus />} />
-              <Route path="/setting" element={<Setting />} />
-            </Routes>
-          </section>
-        </main>
+          </nav>
+          <main>
+            <section id="main-section">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/zone" element={<Zone />} />
+                <Route path="/student" element={<Student />} />
+                <Route path="/teacher" element={<Teacher />} />
+                <Route path="/attendance" element={<Attendance />} />
+                <Route path="/syllabus" element={<Syllabus />} />
+                <Route path="/setting" element={<Setting />} />
+              </Routes>
+            </section>
+          </main>
+        </section>
       </Router>
     </>
   );
