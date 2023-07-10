0a1171ce6c9eb89acce55ad5a53604fe5c9cdd91
reactworkspace <reactworkspace1@gmail.com>
2023-07-10 13:40:04 -0700

    Syllabus Main Page

--- src/pages/syllabus.js
+++ src/pages/syllabus.js
@@ -3,12 +3,16 @@ import '../styles/index.css';
 import '../styles/syllabus.css';
 import '../styles/font.css';
 import '../styles/flex.css';
+import '../styles/color.css';
 
 // Import user profile components
 import UserProfile from '../components/userprofile';
 
 // Import Components
 import { PrimaryButton } from '../components/buttons';
+
+// Import SVG Icon Componenets
+import { ReactComponent as CircleArrow } from '../assets/svg/circle-arrow.svg';
 
 const Syllabus = () => {
   return (
@@ -25,9 +29,72 @@ const Syllabus = () => {
             <div>
               <span className="poppins-sub-heading">all subjects</span>
             </div>
-            <div><PrimaryButton title='add subject' >Hello</PrimaryButton></div>
+            <div>
+              <PrimaryButton title="add subject" />
+            </div>
           </div>
-          <div className="syllabus-subject-list-container"></div>
+          <div className="syllabus-subject-list-container flex-col gap2rem">
+
+            <div className="syllabus-subject-list flex-r-sb">
+              <div className="flex gap2rem">
+                <div className="syllabus-subject-box flex-c">
+                  <span className="poppins-heading color-white">1</span>
+                </div>
+                <div>
+                  <span className="poppins-heading-2rem-600">surah</span>
+                </div>
+              </div>
+             <CircleArrow className='circle-arrow-icon' />
+            </div>
+
+            <div className="syllabus-subject-list flex-r-sb">
+              <div className="flex gap2rem">
+                <div className="syllabus-subject-box flex-c">
+                  <span className="poppins-heading color-white">2</span>
+                </div>
+                <div>
+                  <span className="poppins-heading-2rem-600">ahadees</span>
+                </div>
+              </div>
+              <CircleArrow className='circle-arrow-icon' />
+            </div>
+
+            <div className="syllabus-subject-list flex-r-sb">
+              <div className="flex gap2rem">
+                <div className="syllabus-subject-box flex-c">
+                  <span className="poppins-heading color-white">3</span>
+                </div>
+                <div>
+                  <span className="poppins-heading-2rem-600">dua</span>
+                </div>
+              </div>
+              <CircleArrow className='circle-arrow-icon' />
+            </div>
+
+            <div className="syllabus-subject-list flex-r-sb">
+              <div className="flex gap2rem">
+                <div className="syllabus-subject-box flex-c">
+                  <span className="poppins-heading color-white">4</span>
+                </div>
+                <div>
+                  <span className="poppins-heading-2rem-600">azkaar e namaz</span>
+                </div>
+              </div>
+              <CircleArrow className='circle-arrow-icon' />
+            </div>
+
+            <div className="syllabus-subject-list flex-r-sb">
+              <div className="flex gap2rem">
+                <div className="syllabus-subject-box flex-c">
+                  <span className="poppins-heading color-white">5</span>
+                </div>
+                <div>
+                  <span className="poppins-heading-2rem-600">kalima</span>
+                </div>
+              </div>
+              <CircleArrow className='circle-arrow-icon' />
+            </div>
+          </div>
         </div>
       </div>
     </section>
