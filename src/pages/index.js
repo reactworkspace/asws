98f083997e2c14a658568b84f1206c5e56df4dc6
reactworkspace <reactworkspace1@gmail.com>
2023-08-09 08:18:56 -0700

    hashrouter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 // styles
 import '../assets/css/index.css';
