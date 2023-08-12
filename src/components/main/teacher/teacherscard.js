d2075b19f6683da65c8d9024c279a8b6817f6150
reactworkspace <reactworkspace1@gmail.com>
2023-08-12 23:59:01 +0530

    update: changes

--- src/components/main/teacher/teacherscard.js
+++ src/components/main/teacher/teacherscard.js
@@ -2,31 +2,30 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 //  import profile
-// import TeacherProfile from '../../../assets/images/teacherprofile.png';
+import TeacherProfile from '../../../assets/images/teacherprofile.png';
 
 // react icons
 import { BsTelephone, BsEnvelope } from 'react-icons/bs';
 
 // Importing teacher data from data folder
-// import teacherData from '../../../data/teacherdata';
+import teacherData from '../../../data/teacherdata';
 
-const TeacherCard = (props) => {
-  const {TeacherImg, TeacherName, TeacherCenter} = props;
+const TeacherCard = () => {
   return (
     <Link to="/teacherdetails">
       <div id="teacher-card">
         <div className="teacher-card-profile">
           <img
-            src={TeacherImg}
+            src={TeacherProfile}
             alt="teacherprofile"
             className="teacher-profile-img"
           />
         </div>
         <div className="teacher-card-name poppins">
-          <span>{TeacherName}</span>
+          <span>{teacherData.teacherName}</span>
         </div>
         <div className="teacher-card-zone">
-          <span>{TeacherCenter}</span>
+          <span>{teacherData.centreName}</span>
         </div>
         <div className="teacher-card-contact">
           <div className="teacher-circle">
